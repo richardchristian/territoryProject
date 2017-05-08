23c7f35e63c90dc83f31edeb297f9d266ccd90b8
richardchristian <ri.christian@icloud.com>
2017-05-08 17:23:47 +0200

    dashboard-doughnut component

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { DashboardComponent } from './dashboard.component';
+import { DashboardDoughnutComponent } from './dashboard-doughnut.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
@@ -11,6 +12,9 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
     ChartsModule,
     BsDropdownModule
   ],
-  declarations: [ DashboardComponent ]
+  declarations: [
+    DashboardComponent,
+    DashboardDoughnutComponent,
+  ]
 })
 export class DashboardModule { }
